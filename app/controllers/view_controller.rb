670855c197c2d10c88e14742c940a0995b780f9c
Mark Schafers <J259105@tafe.wa.edu.au>
2017-05-23 11:48:34 +0800

    added langing page and basic sign in functionality

---
+++ app/controllers/view_controller.rb
@@ -0,0 +1,4 @@
+class ViewController < ApplicationController
+	def landing
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :channels, only: [:new, :create, :show, :index]
-  root 'channels#index'
+  root to: 'view#landing'
 end
